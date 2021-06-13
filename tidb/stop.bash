4515870038f3d3bf9009514f78c9ee3590fe6141
Liu Cong <innerr@gmail.com>
2021-06-13 23:16:37 +0800

    add confirm setting for tidb.stop
    Signed-off-by: Liu Cong <innerr@gmail.com>

--- tidb/stop.bash
+++ tidb/stop.bash
@@ -4,5 +4,6 @@ env=`cat "${1}/env"`
 here=`cd $(dirname ${BASH_SOURCE[0]}) && pwd`
 . "${here}/../utils/base.bash" "${env}"
 
+confirm=`confirm_str "${env}"`
 name=`must_env_val "${env}" 'tidb.cluster'`
-tiup cluster stop "${name}"
+tiup cluster stop "${name}"${confirm}
