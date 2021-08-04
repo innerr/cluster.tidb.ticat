00a43fc692f320aa50490e29389515bc9b2520a0
wzru <harryshaunwang@outlook.com>
2021-08-04 19:06:38 +0800

    feat: local bin path

--- tidb/deploy.bash
+++ tidb/deploy.bash
@@ -19,4 +19,54 @@ if [ "${skip_exist}" == 'true' ]; then
 	fi
 fi
 
+ver_path=`expr "${ver}" : '\(.*+\)' || true`
+if [ $ver_path ]; then
+    path=${ver#*+}
+    ver=${ver_path%+}
+else
+	path=
+fi
+
 tiup cluster deploy "${name}" "${ver}" "${yaml}"${confirm}
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    os=linux
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    os=darwin
+else
+    os=win
+fi
+
+case $(uname -m) in
+    i386)   arch="386" ;;
+    i686)   arch="386" ;;
+    x86_64) arch="amd64" ;;
+    arm)    arch="arm64" ;;
+esac
+
+if [ ${path} ]; then
+    if [ -d "${path}" ]; then
+		cd ${path}
+        if [ -x "${path}/tidb-server" ]; then
+            tar -czvf tidb-nightly-${os}-${arch}.tar.gz tidb-server
+            tiup cluster patch "${name}" tidb-nightly-${os}-${arch}.tar.gz -R tidb
+        fi
+        if [ -x "${path}/tikv-server" ]; then
+            tar -czvf tikv-nightly-${os}-${arch}.tar.gz ${path}/tikv-server
+            tiup cluster patch "${name}" tidb-nightly-${os}-${arch}.tar.gz -R tikv
+        fi
+        if [ -x "${path}/pd-server" ]; then
+            tar -czvf pd-nightly-${os}-${arch}.tar.gz ${path}/pd-server
+            tiup cluster patch "${name}" tidb-nightly-${os}-${arch}.tar.gz -R pd
+        fi
+		cd -
+    elif [ -x "${path}" ]; then
+        base=`basename ${path}`
+		dir=`dirname ${path}`
+        role=${base%*-server}
+		cd ${dir}
+        tar -czvf tidb-nightly-${os}-${arch}.tar.gz base
+        tiup cluster patch "${name}" ${role}-nightly-${os}-${arch}.tar.gz -R ${role}
+		cd -
+    fi
+fi
