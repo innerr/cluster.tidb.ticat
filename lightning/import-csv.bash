fefe24b0d93295d27f994daa08f818549fd15a2a
root <root@localhost.localdomain>
2021-10-28 18:22:45 +0800

    minor fixes

--- lightning/import-csv.bash
+++ lightning/import-csv.bash
@@ -12,15 +12,17 @@ tmp_dir=`must_env_val "${env}" 'lightning.tmp-dir'`
 export src_dir=`must_env_val "${env}" 'lightning.data-source-dir'`
 
 uuid=`uuidgen`
+base_dir="${tmp_dir}/lightning-csv-${uuid}"
+echo "Lightning files will be at ${base_dir}"
 
-conf_log_dir="/tmp/lightning-csv-${uuid}"
+conf_log_dir="${base_dir}/conf-log"
 mkdir -p "${conf_log_dir}"
 conf_file="${conf_log_dir}/lightning-csv.toml"
 export log_file="${conf_log_dir}/lightning-csv.log"
-echo "Log file will be at ${log_file}"
 
-export sorted_kv_dir="${tmp_dir}/sorted-kv-dir-${uuid}"
-mkdir -p "${sorted-kv-dir}"
+export sorted_kv_dir="${base_dir}/sorted-kv"
+mkdir -p "${sorted_kv_dir}"
+echo "Sorted KVs will be at ${sorted_kv_dir}"
 
 export host=`must_env_val "${env}" 'mysql.host'`
 export port=`must_env_val "${env}" 'mysql.port'`
